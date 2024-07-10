5ad5c51223705fe4f0347e7eee43b6ab17e72e91
ALprjkt <168145100+ALprjkt@users.noreply.github.com>
2024-07-10 16:16:24 +0000

    vince: Build Lineage Health HAL
    Signed-off-by: ALprjkt <168145100+ALprjkt@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -97,6 +97,9 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/vintf/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/vintf/compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/vintf/framework_manifest.xml
 
+# Lineage Health
+TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH := /sys/class/power_supply/battery/battery_charging_enabled
+
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3221225472
