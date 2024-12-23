a072629174a57c9c5986e5dcb4db03757fce23f2
Michael Bestas <mkbestas@lineageos.org>
2024-12-23 10:29:40 +0000

    vince: Import device_framework_matrix from LineageOS
    *For fix building rom for Non LineageOS Base

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -106,7 +106,7 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
-    vendor/infinity/config/device_framework_matrix.xml
+    $(DEVICE_PATH)/vintf/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/vintf/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/vintf/compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/vintf/framework_manifest.xml
